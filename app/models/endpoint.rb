7c258444aa031f188fc797450e04d5ae00828bd9
Saumya Mehta <smehta1289@gmail.com>
2020-09-18 15:22:15 +0200

    model rspec strutcure

--- app/models/endpoint.rb
+++ app/models/endpoint.rb
@@ -1,29 +1,30 @@
 class Endpoint < ApplicationRecord
-    # attr_accessor :response
+  # attr_accessor :response
 
-    validates :verb, :path, :response_code, presence: true
-    validates :verb, inclusion: { in: %w(GET HEAD POST PUT DELETE CONNECT OPTIONS TRACE),
-    message: "%{value} is not a valid verb" }
-    validates :response_code, numericality: { only_integer: true }
-    validates_uniqueness_of :path, scope: [:verb]
-    validate :valid_path?
+  validates :verb, :path, :response_code, presence: true
+  validates :verb, inclusion: {in: %w(GET HEAD POST PUT DELETE CONNECT OPTIONS TRACE),
+                               message: "%{value} is not a valid verb"}
+  validates :response_code, numericality: {only_integer: true}
+  validates_uniqueness_of :path, scope: [:verb]
+  validate :valid_path?
 
-    before_validation :ensure_upcase_verb
+  before_validation :ensure_upcase_verb
 
-    private
-        def ensure_upcase_verb
-          self.verb = verb.upcase if verb
-        end
+  private
 
-        def valid_path?
-            is_valid = true
-            begin
-                uri = URI.parse(self.path)
-                is_valid = uri.relative?
-            rescue URI::InvalidURIError
-                is_valid = false
-            end
-            errors.add(:path, "invalid path") unless is_valid
-        end
+    def ensure_upcase_verb
+      self.verb = verb.upcase if verb
+    end
+
+    def valid_path?
+      is_valid = true
+      begin
+        uri = URI.parse(self.path)
+        is_valid = uri.relative?
+      rescue URI::InvalidURIError
+        is_valid = false
+      end
+      errors.add(:path, "invalid path") unless is_valid
+    end
 
 end
---
+++ spec/models/endpoint_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+
+RSpec.describe Endpoint do
+
+  describe 'validations' do
+
+  end
+end

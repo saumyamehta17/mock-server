1e8aebc32e276aecabce4b0cce85f4759d5d9649
Saumya Mehta <smehta1289@gmail.com>
2020-09-18 15:50:56 +0200

    rspec for model

--- spec/models/endpoint_spec.rb
+++ spec/models/endpoint_spec.rb
@@ -2,7 +2,42 @@ require "rails_helper"
 
 RSpec.describe Endpoint do
 
-  describe 'validations' do
+  describe '#validations' do
+
+    context '#unsuccessful' do
+      let!(:endpoint) { Endpoint.create(verb: 'GET', path: '/greeting', response_code: 200) }
+
+      it 'returns error when duplicate path' do
+        duplicate_endpoint = Endpoint.create(verb: 'GET', path: '/greeting', response_code: 200)
+        expect(duplicate_endpoint.errors[:path]).to include('has already been taken')
+      end
+
+      it 'returns error without verb' do
+        endpoint_with_no_verb = Endpoint.create(path: '/greetings', response_code: 200)
+        expect(endpoint_with_no_verb.errors[:verb]).to include("can't be blank")
+      end
+
+      it 'returns error with invalid verb' do
+        invalid_endpoint = Endpoint.update(verb: 'FOO')[0]
+        expect(invalid_endpoint.errors[:verb]).to include("FOO is not a valid verb")
+      end
+
+      it 'returns error when invalid response_code' do
+        invalid_endpoint = Endpoint.update(response_code: 'FOO')[0]
+        expect(invalid_endpoint.errors[:response_code]).to include("is not a number")
+      end
+    end
+
+  end
+
+  describe '#methods' do
+    let(:verb) { 'get' }
+    let!(:endpoint) { Endpoint.new(verb: verb, path: '/greeting', response_code: 200) }
+
+    it 'returns upcase verb' do
+      endpoint.valid?
+      expect(endpoint.verb).to eq(verb.upcase)
+    end
 
   end
 end
